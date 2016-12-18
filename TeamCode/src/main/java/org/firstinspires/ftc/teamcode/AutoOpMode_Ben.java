2617c4cb22a96623bf2fb5094a791031f87f7f54
DESKTOP-2F3FBQ4\ChristopherDeloglos <cd.dman@gmail.com>
2016-12-18 15:23:08 -0500

    Added More Motor Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Ben.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Ben.java
@@ -2,13 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; // Add this for default
 
 /**
  * Created by ChristopherDeloglos on 12/18/2016.
  */
 @Autonomous (name = "AutoOpMode_Ben", group = "Autonomous")
 
-public class AutoOpMode_Ben {
+public class AutoOpMode_Ben  extends LinearOpMode {
 
     private DcMotor motorLeft;
     private DcMotor motorRight;
@@ -17,6 +20,14 @@ public class AutoOpMode_Ben {
 
     public void runOpMode() throws InterruptedException
     {
+
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        motorRight.setDirection(DcMotor.Direction.FORWARD);
+
+
 
     }
 }
