e019166ef24dcfb4972ec107d1f3f6aa7d25f042
DESKTOP-2F3FBQ4\ChristopherDeloglos <cd.dman@gmail.com>
2016-12-18 15:11:34 -0500

    Messed up motor
    Added partial functionality for direction turning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode.java
@@ -1,18 +1,19 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode; // Add this for default
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous; // Add this for default if AutoOpMode
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; // Add this for default
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 
 /**
  * Created by ChristopherDeloglos on 12/3/2016.
  * Purpose: Educational model for robot programming
  */
-@Autonomous(name = "AutoOp Mode", group = "Autonomous")
+@Autonomous(name = "AutoOp Mode", group = "Autonomous") // Add this for default (Change name)
 
-public class AutoOpMode extends LinearOpMode
+public class AutoOpMode extends LinearOpMode // Add this for default (Change AutoOpMode to match filename/classname)
 {
     private static final double TICKS_PER_REV = 360; // Number of ticks in one motor turn
     private static final double REVS_PER_METER = 10; // Number of times the wheel turns after the robot moves 1 meter
@@ -20,17 +21,17 @@ public class AutoOpMode extends LinearOpMode
     private static final int OPMODE_NUMBER_PATH_STEPS = 10;
     private static final double DCMOTOR_SPEED_STOPPED = 0;
     private static final double DCMOTOR_SPEED_FULL_SPEED = 1;
+
     private DcMotor motorLeft;
     private DcMotor motorRight;
-    
-    int[] motorLeftStepsTicks = {100, 100, 100, 100, 200, -200, 100, 100, 100, 100};
-    int[] motorRightStepsTicks = {100, 0, 100, 0, 200, -200, 100, 0, 100, 0};
-    double[] motorLeftStepsSpeed = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
-    double[] motorRightStepsSpeed = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int[] thisismyint = {10,20,19};
+    int[] motorLeftStepsTicks =  { 100, 100, 100, 100, 200, -200, 100, 100, 100, 100};
+    int[] motorRightStepsTicks = { 100,   0, 100, 0, 200, -200, 100, 0, 100, 0};
+    double[] motorStepsSpeed =   {   1,   1, 1, 1, 1, 1, 1, 1, .5, 1};
 
-    @Override
-    public void runOpMode() throws InterruptedException
+    public void runOpMode() throws InterruptedException // Add this for default exactly as is
     {
+        //int stuffs = thisismyint[0];
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
 
@@ -40,15 +41,34 @@ public class AutoOpMode extends LinearOpMode
         motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
-        DriveForwardDistance(1,GetTicksFromMeters(1.0));
-        DriveForwardDistance(1,GetTicksFromMeters(1.0));
-        //while(opModeIsActive()){}
 
+        for(int counter = 0;  counter < motorLeftStepsTicks.length;  counter++){
+            TurnDistance(motorStepsSpeed[counter], motorLeftStepsTicks[counter],motorRightStepsTicks[counter]);
+        }
+
+        //while(opModeIsActive()){}
     }
 
-    public void DriveForwardDistance(double power, int distance){
-        ResetEncodersforRunToPosition(distance);
+    public void DriveForwardDistance(double power, int ticks){
+        ResetEncodersforRunToPosition(ticks, ticks);
         DriveFoward(power);
+
+        while(motorLeft.isBusy() || motorRight.isBusy()){
+            if(!motorLeft.isBusy()) {
+                motorLeft.setPower(DCMOTOR_SPEED_STOPPED);
+            }
+            if(!motorRight.isBusy()) {
+                motorRight.setPower(DCMOTOR_SPEED_STOPPED);
+            }
+        }
+        StopMotors();
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void TurnDistance(double power, int ticksLeft,int ticksRight){
+        ResetEncodersforRunToPosition(ticksLeft,ticksRight);
+        Turn(power,power);
 
         while(motorLeft.isBusy() && motorRight.isBusy()){
             if(!motorLeft.isBusy()) {
@@ -63,14 +83,14 @@ public class AutoOpMode extends LinearOpMode
         motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    public void ResetEncodersforRunToPosition(int distance){
+    private void ResetEncodersforRunToPosition(int leftTicks,int rightTicks){
         // Reset encoders
         motorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         // Set target position
-        motorLeft.setTargetPosition(distance);
-        motorRight.setTargetPosition(distance);
+        motorLeft.setTargetPosition(leftTicks);
+        motorRight.setTargetPosition(rightTicks);
 
         // Set to RUN_TO_POSITION mode
         motorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -78,11 +98,16 @@ public class AutoOpMode extends LinearOpMode
 
     }
 
-
     public void DriveFoward(double power) {
         motorLeft.setPower(power);
         motorRight.setPower(power);
     }
+
+    private void Turn(double leftPower, double rightPower){
+        motorLeft.setPower(leftPower);
+        motorRight.setPower(rightPower);
+    }
+
     public void StopMotors(){
         motorLeft.setPower(DCMOTOR_SPEED_STOPPED);
         motorRight.setPower(DCMOTOR_SPEED_STOPPED);
