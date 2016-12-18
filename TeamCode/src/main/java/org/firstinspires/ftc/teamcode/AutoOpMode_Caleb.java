3de0b8dfded61bb00c6f8b0b9e303d7ca0a2762d
Caleb's_Computer <caleb.a.schwartz@gmail.com>
2016-12-18 15:28:21 -0500

    Worked on my program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Caleb.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Caleb.java
@@ -1,8 +1,34 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 /**
  * Created by Caleb Schwartz on 12/18/2016.
  */
 
-public class AutoOpMode_Caleb {
+
+
+@Autonomous(name = "AutoOp Mode_Caleb", group = "Autonomous")
+
+public class AutoOpMode_Caleb extends LinearOpMode{
+
+
+
+
+    private static final double TICKS_PER_REV = 360;
+    private static final double REVS_PER_METER = 10;
+    private static final double GEAR_RATION = 1;
+    private static final double DCMOTOR_SPEED_STOPPED = 0;
+    private static final double DCMOTOR_SPEED_
+    private static final double
+
+
+    @Override
+    private void runOpMode() throws InterruptedException {
+
+
+
+    }
 }
+
