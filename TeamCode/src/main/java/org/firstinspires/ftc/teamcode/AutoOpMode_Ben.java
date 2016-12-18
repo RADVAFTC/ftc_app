e88247cf46ca1db0bd9d833a4dc746a294892996
DESKTOP-2F3FBQ4\ChristopherDeloglos <cd.dman@gmail.com>
2016-12-18 15:11:02 -0500

    Made An Opmode With Motors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Ben.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * Created by ChristopherDeloglos on 12/18/2016.
+ */
+@Autonomous (name = "AutoOpMode_Ben", group = "Autonomous")
+
+public class AutoOpMode_Ben {
+
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+
+
+
+    public void runOpMode() throws InterruptedException
+    {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode_Ben.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by ChristopherDeloglos on 12/18/2016.
+ */
+
+public class TeleOpMode_Ben {
+
+
+}
