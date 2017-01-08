cfd6db90345e1488fb86399976d4910f808240b3
Caleb's_Computer <caleb.a.schwartz@gmail.com>
2017-01-08 11:14:51 -0500

    Worked on my AutoOp program at robotics meeting 1/4/17 (forgot to commit these changes) Caleb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Caleb.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOpMode_Caleb.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Created by Caleb Schwartz on 12/18/2016.
@@ -20,12 +22,33 @@ public class AutoOpMode_Caleb extends LinearOpMode{
     private static final double REVS_PER_METER = 10;
     private static final double GEAR_RATION = 1;
     private static final double DCMOTOR_SPEED_STOPPED = 0;
-    private static final double DCMOTOR_SPEED_
-    private static final double
+    private static final double DCMOTOR_SPEED_FULL = 1;
 
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+    private DcMotor motorFrontArm;
+
+    //These that follow are the steps for autonomous
+
+    int[] leftstepsticks       = {100, 200, 300};
+    int[] rightstepsticks      = {100, 200, 300};
+    double[] motorspeedperstep = {1, 1, 1 };
 
     @Override
-    private void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException {
+
+        motorLeft = hardwareMap.dcMotor.get("motorLeft");
+        motorRight = hardwareMap.dcMotor.get("motorRight");
+
+        motorLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorRight.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontArm.setDirection(DcMotor.Direction.REVERSE);
+
+        for (int counter = 0; counter <leftstepsticks.length; counter++ ){
+
+        }
+
+
 
 
 
