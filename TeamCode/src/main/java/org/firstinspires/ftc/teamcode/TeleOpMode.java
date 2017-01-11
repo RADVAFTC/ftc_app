e0eef31e457ff2ebdd87bcd17542abf837f140b1
DESKTOP-2F3FBQ4\ChristopherDeloglos <cd.dman@gmail.com>
2017-01-11 12:17:17 -0700

    Added motor controller 1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -16,6 +17,7 @@ public class TeleOpMode extends LinearOpMode
     private DcMotor motorLeft;
     private DcMotor motorRight;
     private DcMotor motorLauncher;
+    private DcMotorController motorController1;
 
     private static final double MOTOR_SAFE_SPEED = 0.3;
     private static final double MOTOR_FULL_SPEED = 1;
@@ -23,9 +25,10 @@ public class TeleOpMode extends LinearOpMode
     @Override
     public void runOpMode() throws InterruptedException
     {
+        motorController1 = hardwareMap.dcMotorController.get("Motor Controller 1");
         motorLeft = hardwareMap.dcMotor.get("motorLeft");
         motorRight = hardwareMap.dcMotor.get("motorRight");
-        motorLauncher = hardwareMap.dcMotor.get("motorLauncher");
+        //motorLauncher = hardwareMap.dcMotor.get("motorLauncher");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
 
